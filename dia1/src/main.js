b6b1a2bee4a7833ea52c5b032e8fd6054e67a97a
bazithiago <thiagovieira.dev@gmail.com>
2021-08-17 12:50:50 -0300

    Exercicio04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -3,4 +3,18 @@ import './style.css'
 document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+  `
+  
+  const createLink = document.createElement('a');
+  document.body.appendChild(createLink);
+  document.querySelector('a').innerHTML = `
+    Clique aqui
+  `
+
+  const link = document.querySelector('a');
+  link.addEventListener('click', ()=>{
+    const app = document.querySelector('.app');
+    app.classList.toggle('hidden')
+  }, false)
+
+
