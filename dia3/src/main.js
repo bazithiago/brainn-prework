90120f7693e15ccd275659630058cd625b5e07de
bazithiago <thiagovieira.dev@gmail.com>
2021-08-20 11:04:14 -0300

    Exercicio 01 - apos correcao aula

--- dia3/src/main.js
+++ dia3/src/main.js
@@ -1,20 +1,42 @@
 import './style.css'
 
 ///////// EXERCICIO 1
-const inputName = document.querySelector('[data-js="name"]')
-const exceptions = ['de', 'da', 'do', 'dos']
+// const inputName = document.querySelector('[data-js="name"]')
+// const exceptions = ['de', 'da', 'do', 'dos']
 
-const capitalize = function(e) {
-  e.target.value = inputName.value
-      .toLowerCase()
-      .split(' ')
-      .map(function(word) {
-          return word[0].toUpperCase() + word.substr(1);
-      })
-      .join(' ');
+// const capitalize = function(e) {
+  //   e.target.value = inputName.value
+  //       .toLowerCase()
+//       .split(' ')
+//       .map(function(word) {
+//           return word[0].toUpperCase() + word.substr(1);
+//       })
+//       .join(' ');
+// }
+// inputName.addEventListener('keyup', capitalize)
+
+const inputName = document.querySelector('[data-js="name"]')
+const exceptions = ['da', 'de', 'do', 'das', 'dos']
+
+inputName.addEventListener('input', (e) => {
+  const valueAsArray = e.target.value.split(' ')
+
+  e.target.value = valueAsArray.map((word)=>{
+    return exceptions.includes(word.toLowerCase())
+      ? word.toLowerCase()
+      : fixCase(word)
+  }).join(' ')
+})
+
+function fixCase(word) {
+  if (word.lengh === 0) {
+    return ''
+  }
+  return `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`
 }
 
-inputName.addEventListener('keyup', capitalize)
+
+
 
 ///////// EXERCICIO 2
 const checkboxes = document.querySelectorAll('[data-js="colors-selector"')
